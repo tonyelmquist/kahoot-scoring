3bc110c925794199ae9e613833f448600dff0de9
John Anton Elmquist <john.elmquist@thomsonreuters.com>
2018-04-11 18:08:27 +0200

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import App from './js/containers/App'
+import './css/index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ src/js/constants/items.js
@@ -0,0 +1,32 @@
+const items = [
+        {
+          name: "A",
+          value: 50,
+          bonusInterval: 3,
+          bonusValue: 200,
+          tally: 0
+        },
+        {
+          name: "B",
+          value: 30,
+          bonusInterval: 2,
+          bonusValue: 90,
+          tally: 0
+        },
+        {
+          name: "C",
+          value: 20,
+          bonusInterval: 0,
+          bonusValue: 0,
+          tally: 0
+        },
+        {
+          name: "D",
+          value: 15,
+          bonusInterval: 0,
+          bonusValue: 0,
+          tally: 0
+        }
+      ]
+
+export default items;
